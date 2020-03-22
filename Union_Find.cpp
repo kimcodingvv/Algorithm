f9517b4b880e2a53155352ac732f8f91d04f5ce1
Kimcoding <junghun1997@naver.com>
2020-03-23 04:44:11 +0900

    Create Union_Find.cpp

---
+++ Union_Find.cpp
@@ -0,0 +1,19 @@
+struct UF {
+	int *arr, *sz;
+	UF(int n) {
+		arr = new int[n + 1];
+		sz = new int[n + 1];
+		FOR(i, 0, n) arr[i] = i, sz[i] = 1;
+	}
+	int find(int x) {
+		return (arr[x] == x) ? x : x = find(arr[x]);
+	}
+	void merge(int a, int b) {
+		a = find(a), b = find(b);
+		if (sz[a] > sz[b]) swap(a, b);
+		arr[a] = b, sz[b] += sz[a];
+	}
+	~UF() {
+		delete[] arr, delete[] sz;
+	}
+};
